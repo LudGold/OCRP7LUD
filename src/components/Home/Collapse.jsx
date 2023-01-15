a0a8fbfc08508de6c0fbb98463e3ceb7e2ab5a88
Goldstein Ludivine <sneakymombox@gmail.com>
2023-01-15 19:35:16 +0100

    css review on collapse and oneflat

--- src/components/Home/Collapse.jsx
+++ src/components/Home/Collapse.jsx
@@ -14,7 +14,7 @@ export default function Collapse(props) {
           type="button"
           onClick={() => setIsOpen(false)}
         >
-          <img src="/arrow_up.png" alt="flèche du haut" />
+          <img className= "chevron_haut" src="/arrow_up.png" alt="flèche du haut" />
         </button>
       </div>
           <div className="collapse_open_block"><p className="collapse-description">{props.description}</p></div>
@@ -29,7 +29,7 @@ export default function Collapse(props) {
           type="button"
           onClick={() => setIsOpen(true)}
         >
-          <img src="/arrow_down.png" alt="flèche du bas" />
+          <img  className= "chevron_bas" src="/arrow_down.png" alt="flèche du bas" />
         </button>
       </div>
     </div>
