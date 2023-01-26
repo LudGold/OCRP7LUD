47004d0b4df8523136c019611671dc3e0d17e66a
Goldstein Ludivine <sneakymombox@gmail.com>
2023-01-26 17:43:53 +0100

    update folder accueil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "../components/layout/Header";
 import Footer from "../components/layout/Footer";
-import Banner from "../components/home/Banner";
-import Card from "../components/home/Card";
+import Banner from "../components/accueil/Banner";
+import Card from "../components/accueil/Card";
 
 export default function Home() {
   return (
